a6c3581f388dd96b2bfb84514581f7add8db9e0c
mohammedanaz <mohammedanaz@gmail.com>
2024-07-24 10:33:01 +0530

    custome pagination added

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', home),
+    path('list_user/', ListUser.as_view(), name='list_user'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics
+from .pagination import CustomPagination
+from django.contrib.auth.models import User
+from .serialiser import UserSerialiser
 
 @api_view(['GET', 'POST'])
 def home(request):
@@ -7,4 +11,9 @@ def home(request):
         return Response({'status': 200, 'message': 'Hello DRF from GET'})
     if request.method == 'POST':
         return Response({'status': 200, 'message': 'Hello DRF from POST'})
+    
+class ListUser(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerialiser
+    pagination_class = CustomPagination
     
